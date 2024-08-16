1a420cf80a578546176084341ea206f16565f340
rloredo <rodrigo.loredo@vinted.com>
2024-08-17 00:26:00 +0200

    fix models

--- customers/models/dimensions/dim_awfuls.sql
+++ customers/models/dimensions/dim_awfuls.sql
@@ -1,3 +1,8 @@
+{{
+    config(
+        unique_key="customer_id",
+    )
+}}
 with customers as (select customer_id, first_name, last_name from {{ ref("stg_customers") }})
 select *
 from customers
