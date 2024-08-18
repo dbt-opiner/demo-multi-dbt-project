76c12030d72bc1e50b400d533c886508ae79d657
rloredo <rodrigo.loredo@vinted.com>
2024-08-18 13:34:19 +0200

    fix critical

--- customers/models/dimensions/dim_customers/dim_customers.sql
+++ customers/models/dimensions/dim_customers/dim_customers.sql
@@ -1,1 +1,3 @@
-with customers as (select * from {{ ref("stg_customers") }}) select * from customers
+with customers as (select customer_id, first_name, last_name from {{ ref("stg_customers") }})
+select *
+from customers
